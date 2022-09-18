76000ad8b9fba9a996c2f52e7608d382bb5a7206
Your Name <you@example.com>
2022-09-19 03:20:15 +0600

    now its time to handle login page

---
+++ src/Pages/Shared/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Sorry This page can't found</h2>
+        </div>
+    );
+};
+
+export default NotFound;
